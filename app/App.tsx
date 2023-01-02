19ef58e5a0d20d490a7cf2b11dae90b42a9ed822
Rafael Luiz <luizrafael391@gmail.com>
2023-01-02 15:09:29 -0300

    feat: app add poppins font

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 
 import MainStack from './src/stacks/MainStack';
 
+const customFonts = {
+  'Poppins-Regular': require('./src/assets/fonts/Poppins-Regular.ttf'),
+  'Poppins-Bold': require('./src/assets/fonts/Poppins-Bold.ttf'),
+};
+
 export default function App() {
+  useFonts(customFonts);
+
   return (
     <NavigationContainer>
       <MainStack />
