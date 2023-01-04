bd6b374a112edd507d1d73117a2e69311fdc5646
Rafael Luiz <luizrafael391@gmail.com>
2023-01-04 12:00:47 -0300

    fix: backend remove SimpleInfoProvider and AwsSqsProvider from provider/index.ts

--- backend/src/infra/providers/index.ts
+++ backend/src/infra/providers/index.ts
@@ -2,5 +2,3 @@ export * from './BcryptProvider';
 export * from './EmailValidatorProvider';
 export * from './CpfCnpjValidatorProvider';
 export * from './JwtProvider';
-export * from './SimpleInfoProvider';
-export * from './AwsSqsProvider';
