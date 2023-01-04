f67fd21e23a29d9d022fb1f42e5030bef6308f3f
Rafael Luiz <luizrafael391@gmail.com>
2023-01-04 11:57:33 -0300

    feat: backend ensure can only create service if user is provider

--- backend/src/controllers/CreateServiceController.ts
+++ backend/src/controllers/CreateServiceController.ts
@@ -1,14 +1,22 @@
-import { MissingParamError } from '@/errors';
-import { badRequest, ok } from '@/helpers';
-import { Controller, HttpResponse, IServiceRepository } from '@/protocols';
+import { MissingParamError, NotFoundError } from '@/errors';
+import { badRequest, notFound, ok } from '@/helpers';
+import {
+  Controller,
+  HttpResponse,
+  IProviderRepository,
+  IServiceRepository,
+} from '@/protocols';
 
 export class CreateServiceController implements Controller {
-  constructor(private serviceRepository: IServiceRepository) {}
+  constructor(
+    private serviceRepository: IServiceRepository,
+    private providerRepository: IProviderRepository
+  ) {}
 
   async handle(
     request: CreateServiceController.Request
   ): Promise<HttpResponse> {
-    const required = ['provider_id', 'value', 'name'];
+    const required = ['value', 'name'];
 
     for (const field of required) {
       if (!request[field]) {
@@ -16,10 +24,16 @@ export class CreateServiceController implements Controller {
       }
     }
 
-    const { name, provider_id, value } = request;
+    const provider = await this.providerRepository.findById(request.userId);
+
+    if (!provider) {
+      return notFound(new NotFoundError('provider'));
+    }
+
+    const { name, value, userId } = request;
     const service = await this.serviceRepository.add({
       name,
-      provider_id,
+      provider_id: userId,
       value,
     });
 
@@ -29,7 +43,7 @@ export class CreateServiceController implements Controller {
 
 export namespace CreateServiceController {
   export type Request = {
-    provider_id: string;
+    userId: string;
     name: string;
     value: number;
   };
--- backend/src/main/factories/controllers/CreateServiceControllerFactory.ts
+++ backend/src/main/factories/controllers/CreateServiceControllerFactory.ts
@@ -1,8 +1,9 @@
 import { CreateServiceController } from '@/controllers';
-import { DynamoServiceRepository } from '@/infra/db';
+import { DynamoServiceRepository, DynamoProviderRepository } from '@/infra/db';
 
 export const makeCreateServiceController = () => {
   const serviceRepository = new DynamoServiceRepository();
+  const providerRepository = new DynamoProviderRepository();
 
-  return new CreateServiceController(serviceRepository);
+  return new CreateServiceController(serviceRepository, providerRepository);
 };
