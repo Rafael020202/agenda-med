9d8b6abe65f0b61bb2c2752fd2f8d881f9104688
Rafael Luiz <luizrafael391@gmail.com>
2023-01-04 12:13:39 -0300

    feat: backend ensure cannot list schedule with a not exist provider

--- backend/src/controllers/ListScheduleController.ts
+++ backend/src/controllers/ListScheduleController.ts
@@ -1,10 +1,25 @@
-import { ok } from '@/helpers';
-import { Controller, HttpResponse, IScheduleRepository } from '@/protocols';
+import { NotFoundError } from '@/errors';
+import { notFound, ok } from '@/helpers';
+import {
+  Controller,
+  HttpResponse,
+  IScheduleRepository,
+  IProviderRepository,
+} from '@/protocols';
 
 export class ListScheduleController implements Controller {
-  constructor(private scheduleRepository: IScheduleRepository) {}
+  constructor(
+    private scheduleRepository: IScheduleRepository,
+    private providerRepository: IProviderRepository
+  ) {}
 
   async handle(request: ListScheduleController.Request): Promise<HttpResponse> {
+    const provider = await this.providerRepository.findById(request.providerId);
+
+    if (!provider) {
+      return notFound(new NotFoundError('provider'));
+    }
+
     const schedule = await this.scheduleRepository.listByProviderId(
       request.providerId
     );
--- backend/src/main/factories/controllers/ListScheduleControllerFactory.ts
+++ backend/src/main/factories/controllers/ListScheduleControllerFactory.ts
@@ -1,8 +1,9 @@
 import { ListScheduleController } from '@/controllers';
-import { DynamoScheduleRepository } from '@/infra/db';
+import { DynamoScheduleRepository, DynamoProviderRepository } from '@/infra/db';
 
 export const makeListScheduleController = () => {
   const scheduleRepository = new DynamoScheduleRepository();
+  const providerRepository = new DynamoProviderRepository();
 
-  return new ListScheduleController(scheduleRepository);
+  return new ListScheduleController(scheduleRepository, providerRepository);
 };
