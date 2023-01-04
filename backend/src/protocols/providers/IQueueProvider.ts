ff9147551eade94ca28346a00c3c020c58ae6268
Rafael Luiz <luizrafael391@gmail.com>
2023-01-04 12:01:18 -0300

    chore: backend remove QueueProvider

--- backend/src/protocols/providers/IQueueProvider.ts
+++
@@ -1,5 +0,0 @@
-import { Topics } from '@/config/queue';
-
-export interface IQueueProvider {
-  add(topic: keyof typeof Topics, message: any): Promise<void>;
-}
--- backend/src/protocols/providers/index.ts
+++ backend/src/protocols/providers/index.ts
@@ -2,5 +2,4 @@ export * from './IHashProvider';
 export * from './IEmailValidatorProvider';
 export * from './IDocumentValidatorProvider';
 export * from './IEncrypterProvider';
-export * from './IQueueProvider';
 export * from './IDecrypterProvider';
