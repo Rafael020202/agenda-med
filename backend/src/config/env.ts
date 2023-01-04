e4c674cdb4b125c9e2bcceb7009ef41803e6ffa9
Rafael Luiz <luizrafael391@gmail.com>
2023-01-04 11:33:34 -0300

    chore: backend remove company table aws resource

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,6 +1,5 @@
 const env = {
   UsersTableName: process.env.USERS_TABLE,
-  CompanyTableName: process.env.COMPANY_TABLE,
   ServiceTableName: process.env.SERVICE_TABLE,
   ScheduleTableName: process.env.SCHEDULE_TABLE,
   AppointmentTableName: process.env.APPOINTMENT_TABLE,
